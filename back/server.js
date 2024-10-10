bb589b3c5fb2c7803bbe9d3f50f5d0088c51aaea
Chaymae <chaymae.ezzahri@gmail.com>
2024-10-10 23:12:28 +0200

    feat(#1): add js compiler

--- back/server.js
+++ back/server.js
@@ -1,45 +1,53 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { exec } = require('child_process');
-const fs = require('fs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { exec } = require("child_process");
+const fs = require("fs");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(bodyParser.json());
-app.use(cors()); 
+app.use(cors());
 
-app.post('/run-code', (req, res) => {
+app.post("/run-code-py", (req, res) => {
   const { code } = req.body;
 
- 
-  fs.writeFileSync('user_code.py', code);
+  fs.writeFileSync("user_code.py", code);
 
- 
-  exec('py user_code.py', (err, stdout, stderr) => {
+  exec("py user_code.py", (err, stdout, stderr) => {
     if ((err || stderr) && !stdout) {
       return res.json({
         output: stderr,
         isCorrect: false,
       });
-    }else {
+    } else {
       return res.json({
         output: stdout,
         isCorrect: true,
       });
     }
-
-    
-    const expectedOutput = 'Hello, World!\n';
-    const isCorrect = stdout === expectedOutput;
-
-    res.json({
-      output: stdout,
-      isCorrect: isCorrect,
-    });
   });
 });
 
+app.post("/run-code-js", (req, res) => {
+  const { code } = req.body;
+
+  fs.writeFileSync("user_code.js", code);
+
+  exec("node user_code.js", (err, stdout, stderr) => {
+    if ((err || stderr) && !stdout) {
+      return res.json({
+        output: stderr,
+        isCorrect: false,
+      });
+    } else {
+      return res.json({
+        output: stdout,
+        isCorrect: true,
+      });
+    }
+  });
+});
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
--- front/App.js
+++ front/App.js
@@ -1,28 +1,50 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import MonacoEditor from 'react-monaco-editor';
-import axios from 'axios';
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import MonacoEditor from "react-monaco-editor";
+import axios from "axios";
 
 const App = () => {
-  const [code, setCode] = useState(''); 
-  const [output, setOutput] = useState(''); 
-  const [correction, setCorrection] = useState(''); 
+  const [code, setCode] = useState("");
+  const [output, setOutput] = useState("");
+  const [correction, setCorrection] = useState("");
 
-  const handleRunCode = async () => {
+  const [codeJS, setCodeJS] = useState("");
+  const [outputJS, setOutputJS] = useState("");
+  const [correctionJS, setCorrectionJS] = useState("");
+
+  const handleRunCodePY = async () => {
     try {
-     
-      const res = await axios.post('http://localhost:3000/run-code', { code });
-      setOutput(res.data.output); 
+      const res = await axios.post("http://localhost:3000/run-code-py", {
+        code,
+      });
+      setOutput(res.data.output);
 
-     
       if (res.data.isCorrect) {
-        setCorrection('Bravo, votre code est correct !');
+        setCorrection("Bravo, votre code est correct !");
       } else {
-        setCorrection('Votre code contient des erreurs.');
+        setCorrection("Votre code contient des erreurs.");
       }
     } catch (error) {
       console.error(error);
-      setOutput('Erreur lors de l’exécution du code.');
+      setOutput("Erreur lors de l’exécution du code.");
+    }
+  };
+
+  const handleRunCodeJS = async () => {
+    try {
+      const res = await axios.post("http://localhost:3000/run-code-js", {
+        code: codeJS,
+      });
+      setOutputJS(res.data.output);
+
+      if (res.data.isCorrect) {
+        setCorrectionJS("Bravo, votre code est correct !");
+      } else {
+        setCorrectionJS("Votre code contient des erreurs.");
+      }
+    } catch (error) {
+      console.error(error);
+      setOutputJS("Erreur lors de l’exécution du code.");
     }
   };
 
@@ -36,13 +58,13 @@ const App = () => {
         theme="vs-dark"
         value={code}
         options={{ selectOnLineNumbers: true }}
-        onChange={(newValue) => setCode(newValue)} 
+        onChange={(newValue) => setCode(newValue)}
         editorDidMount={(editor) => editor.focus()}
         height="200"
       />
 
       {}
-      <Button title="Exécuter le code" onPress={handleRunCode} />
+      <Button title="Exécuter le code" onPress={handleRunCodePY} />
 
       {}
       <Text style={styles.outputTitle}>Résultat :</Text>
@@ -50,6 +72,30 @@ const App = () => {
 
       {}
       <Text style={styles.correction}>{correction}</Text>
+
+      <Text style={styles.title}>
+        Test Javascript: Imprimez "Hello, World!"
+      </Text>
+
+      {}
+      <MonacoEditor
+        language="javascript"
+        theme="vs-dark"
+        value={codeJS}
+        options={{ selectOnLineNumbers: true }}
+        onChange={(newValue) => setCodeJS(newValue)}
+        height="200"
+      />
+
+      {}
+      <Button title="Exécuter le code" onPress={handleRunCodeJS} />
+
+      {}
+      <Text style={styles.outputTitle}>Résultat :</Text>
+      <Text style={styles.output}>{outputJS}</Text>
+
+      {}
+      <Text style={styles.correction}>{correctionJS}</Text>
     </View>
   );
 };
@@ -58,26 +104,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#f0f8ff',
+    backgroundColor: "#f0f8ff",
   },
   title: {
     fontSize: 20,
     marginBottom: 10,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   outputTitle: {
     marginTop: 20,
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   output: {
     fontSize: 16,
-    color: '#333',
+    color: "#333",
     marginBottom: 10,
   },
   correction: {
     fontSize: 16,
-    color: '#e74c3c',
+    color: "#e74c3c",
   },
 });
 
