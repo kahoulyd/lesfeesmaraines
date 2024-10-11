97a05ac8bc1c74a05bc4c8bdfe1a1308bfde28ec
Chaymae <chaymae.ezzahri@gmail.com>
2024-10-12 01:01:34 +0200

    feat(#2): joli application giiirls

--- front/App.js
+++ front/App.js
@@ -1,174 +1,19 @@
-import React, { useState } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import MonacoEditor from "react-monaco-editor";
-import axios from "axios";
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Compiler from "./Compiler";
+import Dashboard from "./Dashboard";
 
-const App = () => {
-  const [code, setCode] = useState("");
-  const [output, setOutput] = useState("");
-  const [correction, setCorrection] = useState("");
+const Stack = createStackNavigator();
 
-  const [codeJS, setCodeJS] = useState("");
-  const [outputJS, setOutputJS] = useState("");
-  const [correctionJS, setCorrectionJS] = useState("");
-
-  const [codePHP, setCodePHP] = useState("");
-  const [outputPHP, setOutputPHP] = useState("");
-  const [correctionPHP, setCorrectionPHP] = useState("");
-
-  const handleRunCodePY = async () => {
-    try {
-      const res = await axios.post("http://localhost:3000/run-code-py", {
-        code,
-      });
-      setOutput(res.data.output);
-
-      if (res.data.isCorrect) {
-        setCorrection("Bravo, votre code est correct !");
-      } else {
-        setCorrection("Votre code contient des erreurs.");
-      }
-    } catch (error) {
-      console.error(error);
-      setOutput("Erreur lors de l’exécution du code.");
-    }
-  };
-
-  const handleRunCodeJS = async () => {
-    try {
-      const res = await axios.post("http://localhost:3000/run-code-js", {
-        code: codeJS,
-      });
-      setOutputJS(res.data.output);
-
-      if (res.data.isCorrect) {
-        setCorrectionJS("Bravo, votre code est correct !");
-      } else {
-        setCorrectionJS("Votre code contient des erreurs.");
-      }
-    } catch (error) {
-      console.error(error);
-      setOutputJS("Erreur lors de l’exécution du code.");
-    }
-  };
-
-  const handleRunCodePHP = async () => {
-    try {
-      const res = await axios.post("http://localhost:3000/run-code-php", {
-        code : codePHP,
-      });
-      setOutputPHP(res.data.output);
-
-      if (res.data.isCorrect) {
-        setCorrectionPHP("Bravo, votre code est correct !");
-      } else {
-        setCorrectionPHP("Votre code contient des erreurs.");
-      }
-    } catch (error) {
-      console.error(error);
-      setOutputPHP("Erreur lors de l’exécution du code.");
-    }
-  };
-
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Test Python: Imprimez "Hello, World!"</Text>
-
-      {}
-      <MonacoEditor
-        language="python"
-        theme="vs-dark"
-        value={code}
-        options={{ selectOnLineNumbers: true }}
-        onChange={(newValue) => setCode(newValue)}
-        editorDidMount={(editor) => editor.focus()}
-        height="200"
-      />
-
-      {}
-      <Button title="Exécuter le code" onPress={handleRunCodePY} />
-
-      {}
-      <Text style={styles.outputTitle}>Résultat :</Text>
-      <Text style={styles.output}>{output}</Text>
-
-      {}
-      <Text style={styles.correction}>{correction}</Text>
-
-      <Text style={styles.title}>
-        Test Javascript: Imprimez "Hello, World!"
-      </Text>
-
-      {}
-      <MonacoEditor
-        language="javascript"
-        theme="vs-dark"
-        value={codeJS}
-        options={{ selectOnLineNumbers: true }}
-        onChange={(newValue) => setCodeJS(newValue)}
-        height="200"
-      />
-
-      {}
-      <Button title="Exécuter le code" onPress={handleRunCodeJS} />
-
-      {}
-      <Text style={styles.outputTitle}>Résultat :</Text>
-      <Text style={styles.output}>{outputJS}</Text>
-
-      {}
-      <Text style={styles.correction}>{correctionJS}</Text>
-
-      <Text style={styles.title}>Test PHP: Imprimez "Hello, World!"</Text>
-
-      {}
-      <MonacoEditor
-        language="php"
-        theme="vs-dark"
-        value={codePHP}
-        options={{ selectOnLineNumbers: true }}
-        onChange={(newValue) => setCodePHP(newValue)}
-        height="200"
-      />
-
-      {}
-      <Button title="Exécuter le code" onPress={handleRunCodePHP} />
-
-      {}
-      <Text style={styles.outputTitle}>Résultat :</Text>
-      <Text style={styles.output}>{outputPHP}</Text>
-
-      {}
-      <Text style={styles.correction}>{correctionPHP}</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Dashboard} />
+        <Stack.Screen name="Compiler" component={Compiler} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: "#f0f8ff",
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 10,
-    fontWeight: "bold",
-  },
-  outputTitle: {
-    marginTop: 20,
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  output: {
-    fontSize: 16,
-    color: "#333",
-    marginBottom: 10,
-  },
-  correction: {
-    fontSize: 16,
-    color: "#e74c3c",
-  },
-});
-
-export default App;
+}
